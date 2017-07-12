eb1e419b6c1acaea8eb6cd82aa3439a233d85332
Your Name <you@example.com>
2017-07-12 16:52:01 +0100

    RESI-1-basic-layouts, after comments 3

--- app/src/main/java/com/example/lhi06/residue/MainActivity.java
+++ app/src/main/java/com/example/lhi06/residue/MainActivity.java
@@ -18,28 +18,28 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.add_review) FloatingActionButton addReviewButton;
     @BindView(R.id.request) EditText searchField;
     @BindView(R.id.searchButton) Button searchButton;
+    @BindView(R.id.toolbar) Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                TODO: Code for searching for reviews done
-//                String artistName = editText1.getText().toString();
-//                handler.urlBuilder(artistName);
+                //TODO: Code for searching for reviews done
+                //String artistName = editText1.getText().toString();
+                //handler.urlBuilder(artistName);
             }
         });
 
         addReviewButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//          TODO: Navigate to the album searching activity
+                //TODO: Navigate to the album searching activity
 
             }
         });
@@ -48,19 +48,19 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-//         Inflate the menu; this adds items to the action bar if it is present.
+         //Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-//         Handle action bar item clicks here. The action bar will
-//         automatically handle clicks on the Home/Up button, so long
-//         as you specify a parent activity in AndroidManifest.xml.
+         //Handle action bar item clicks here. The action bar will
+         //automatically handle clicks on the Home/Up button, so long
+         //as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-//        noinspection SimplifiableIfStatement
+         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
