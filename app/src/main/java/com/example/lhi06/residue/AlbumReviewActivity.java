151a372eff54315129ea663a3c971183268abccb
Your Name <you@example.com>
2017-07-24 15:36:24 +0100

    RESI-29 Broken & Confused

--- app/src/main/java/com/example/lhi06/residue/AlbumReviewActivity.java
+++ app/src/main/java/com/example/lhi06/residue/AlbumReviewActivity.java
@@ -2,11 +2,18 @@ package com.example.lhi06.residue;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -16,17 +23,36 @@ public class AlbumReviewActivity extends AppCompatActivity {
     @BindView(R.id.textview_album_name_review) TextView albumName;
     @BindView(R.id.textview_artist_name_review) TextView artistName;
     @BindView(R.id.imageview_album_art_review) ImageView albumArtwork;
+    @BindView(R.id.ratingBar) RatingBar ratingBar;
+    @BindView(R.id.reviewText) EditText reviewText;
+    @BindView(R.id.floatingbutton_savereview) FloatingActionButton saveReviewButton;
+
+    SaveState state;
+    List<AlbumReview>reviews;
 
     @Override
     protected void onCreate(@Nullable final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_album_review);
         ButterKnife.bind(this);
-
+        state.getInstance();
+        reviews = state.loadData(this);
+        if (reviews == null) {
+            reviews = new ArrayList<>();
+            state.saveData(reviews, this);
+        }
         final Album album = getIntent().getExtras().getParcelable(FindAlbumActivity.ALBUM_EXTRA);
         albumName.setText(album.getCollectionName());
         artistName.setText(album.getArtistName());
+
         Picasso.with(this).load(album.getArtworkUrl()).into(albumArtwork);
+        saveReviewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                reviews.add(new AlbumReview(album, Float.floatToIntBits(ratingBar.getRating()), reviewText.getText().toString()));
+                state.saveData(reviews, getApplicationContext());
+            }
+        });
     }
 
 }
--- app/src/main/java/com/example/lhi06/residue/MainActivity.java
+++ app/src/main/java/com/example/lhi06/residue/MainActivity.java
@@ -6,8 +6,15 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.EditText;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -15,14 +22,32 @@ import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
+    SaveState state;
+    ReviewAdapter adapter;
+
     @BindView(R.id.floatingbutton_addreview) FloatingActionButton addReviewButton;
     @BindView(R.id.edittext_artist_name) EditText searchField;
+    @BindView(R.id.recycleview_reviews) RecyclerView recyclerView;
+
+    List<AlbumReview> reviews;
 
     @Override
     protected void onCreate(@Nullable final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new ReviewAdapter(Picasso.with(this));
+        recyclerView.setAdapter(adapter);
+        state.getInstance();
+        reviews = state.loadData(this);
+        if (reviews == null) {
+            reviews = new ArrayList<>();
+            state.saveData(reviews, this);
+        }
+        adapter.setListContent(reviews);
+        //TODO: preview old review onclick listener here
+
         addReviewButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(@NonNull final View view) {
